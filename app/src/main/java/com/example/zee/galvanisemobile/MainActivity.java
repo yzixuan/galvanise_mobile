50e391c5ba9efe33d680266e6b5c5a610ee18e86
yzixuan <aura.chrome@gmail.com>
2015-10-08 11:53:54 +0800

    add navigation drawer side menu

--- app/src/main/java/com/example/zee/galvanisemobile/MainActivity.java
+++ app/src/main/java/com/example/zee/galvanisemobile/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zee.galvanisemobile;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
     }
 
     @Override
