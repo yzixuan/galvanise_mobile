cb481bceb31d64cc7cb061bc2d7cec88fc1a1873
yzixuan <aura.chrome@gmail.com>
2015-10-10 17:36:50 +0800

    add food quantity dialog

--- app/src/main/java/com/example/zee/galvanisemobile/ItemDetailActivity.java
+++ app/src/main/java/com/example/zee/galvanisemobile/ItemDetailActivity.java
@@ -1,13 +1,18 @@
 package com.example.zee.galvanisemobile;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
@@ -16,6 +21,8 @@ public class ItemDetailActivity extends AppCompatActivity {
     ImageView imageView;
     TextView foodNameText;
     TextView priceLabel;
+    Button addCartButton;
+    EditText quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +40,43 @@ public class ItemDetailActivity extends AppCompatActivity {
         foodNameText = (TextView)findViewById(R.id.foodNameText);
         priceLabel = (TextView)findViewById(R.id.priceText);
         imageView = (ImageView)findViewById(R.id.image);
+        addCartButton = (Button)findViewById(R.id.add_to_cart);
 
         foodNameText.setText(food.getName());
         priceLabel.setText("$" + String.format("%.2f", food.getPromoPrice()));
 
         imageView.setImageResource(food.getThumbnail());
+
+        // add button listener
+        addCartButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                final Dialog dialog = new Dialog(ItemDetailActivity.this);
+                dialog.setContentView(R.layout.dialog_add_to_cart);
+                dialog.setTitle("Select Quantity");
+
+                Button button = (Button) dialog.findViewById(R.id.confirm_add);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+
+                        quantity = (EditText)dialog.findViewById(R.id.quantity);
+                        Toast.makeText(ItemDetailActivity.this, quantity.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                        dialog.dismiss();
+
+                    }
+                });
+
+
+                dialog.show();
+
+
+            }
+        });
+
+
     }
 
     @Override
@@ -70,5 +109,9 @@ public class ItemDetailActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick_quantity(View view) {
+        Toast.makeText(this, quantity.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
