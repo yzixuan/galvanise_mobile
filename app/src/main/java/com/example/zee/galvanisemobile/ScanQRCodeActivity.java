124b66c9859269b14fa35a3a03247964c47c61a3
yzixuan <aura.chrome@gmail.com>
2015-10-17 22:15:09 +0800

    Lock the QR code scanning to portrait.

---
+++ app/src/main/java/com/example/zee/galvanisemobile/ScanQRCodeActivity.java
@@ -0,0 +1,9 @@
+package com.example.zee.galvanisemobile;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by zee on 17/10/15.
+ */
+public class ScanQRCodeActivity extends CaptureActivity {
+}
