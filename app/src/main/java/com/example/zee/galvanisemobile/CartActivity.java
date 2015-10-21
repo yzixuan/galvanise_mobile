7d4797c475d76f17f5e0ea568928c578ac9090d5
yzixuan <aura.chrome@gmail.com>
2015-10-21 14:27:05 +0800

    remove placeholder images

--- app/src/main/java/com/example/zee/galvanisemobile/CartActivity.java
+++ app/src/main/java/com/example/zee/galvanisemobile/CartActivity.java
@@ -1,7 +1,5 @@
 package com.example.zee.galvanisemobile;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -98,45 +93,6 @@ public class CartActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, QrInstructionsActivity.class);
         startActivity(intent);
-
-        /*IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setCaptureActivity(ScanQRCodeActivity.class);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-        integrator.setOrientationLocked(true);
-        integrator.setPrompt("Scan the table number");
-        integrator.setCameraId(0);  // Use a specific camera of the device
-        integrator.setBeepEnabled(false);
-        integrator.initiateScan();*/
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult barcodeResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (barcodeResult != null) {
-            handleTableNumberScanned(barcodeResult.getContents());
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    private void handleTableNumberScanned(String qrCode) {
-        if (qrCode.matches("[-+]?\\d*\\.?\\d+")) {
-            Intent intent = new Intent(this, PrePayPalActivity.class);
-            intent.putExtra("tableQRCode", qrCode);
-            startActivity(intent);
-        } else {
-
-            new AlertDialog.Builder(this)
-                    .setTitle("Invalid Table Number")
-                    .setCancelable(false)
-                    .setMessage("Sorry, are you sure you're seated inside Galvanise Cafe? please try scanning your table number's QR Code again.")
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-
-                        }
-                    }).show();
-        }
-
     }
 
     private void startShareActivity(String subject, String text) {
